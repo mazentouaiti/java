803dddd3b3c4a14692ce6528eae5d7fc6d2d92e7
maze_n <130292910+mazentouaiti@users.noreply.github.com>
2025-01-30 18:41:13 +0100

    Add files via upload

--- java/ZooManagement.java
+++ java/ZooManagement.java
@@ -24,6 +24,20 @@ public class ZooManagement {
         } finally {
             scanner.close();
         }
+        Animal lion = new Animal("felidae","lion",10,true);
+        Zoo myZoo = new Zoo(zooName,"tunis",nbrCages);
+        System.out.println("Animal ajouté: " + lion.name + " de la famille " + lion.family + ", âge: " + lion.age + ", mammifère: " + lion.isMammal);
+        System.out.println("Zoo créé: " + myZoo.name + " situé à " + myZoo.city + " avec " + myZoo.nbrCages + " cages.");
+
+
+        Animal tiger = new Animal("Felidae", "Tiger", 4, true);
+        Animal elephant = new Animal("Elephantidae", "Elephant", 10, true);
+        Animal crocodile = new Animal("Crocodylidae", "Crocodile", 7, false);
+        myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+        System.out.println(lion);
+        System.out.println(lion.toString());
     }
 }
 
